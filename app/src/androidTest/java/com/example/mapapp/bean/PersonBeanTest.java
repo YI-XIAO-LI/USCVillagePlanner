a9d95c3951cf314cdf478f3664444a065ee54626
Yixiao Li <likather@usc.edu>
2022-11-11 20:40:01 -0800

    Person test Revised

--- app/src/androidTest/java/com/example/mapapp/bean/PersonBeanTest.java
+++ app/src/androidTest/java/com/example/mapapp/bean/PersonBeanTest.java
@@ -13,34 +13,34 @@ public class PersonBeanTest extends TestCase {
 
     public void testTestGetName() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
-        assertEquals(p.getName(), "name");
+        assertEquals("name", p.getName());
     }
 
     public void testTestSetName() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
         p.setName("new_name");
-        assertEquals(p.getName(), "new_name");
+        assertEquals("new_name", p.getName());
     }
 
     public void testGetLatitude() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
-        assertEquals(p.getLatitude(), 35.0);
+        assertEquals(35.0, p.getLatitude());
     }
 
     public void testSetLatitude() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
         p.setLatitude(40.0);
-        assertEquals(p.getLatitude(), 40.0);
+        assertEquals(40.0, p.getLatitude());
     }
 
     public void testGetLongtitude() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
-        assertEquals(p.getLongtitude(), 118.0);
+        assertEquals(118.0, p.getLongtitude());
     }
 
     public void testSetLongtitude() {
         PersonBean p = new PersonBean("name", 35.0, 118.0);
         p.setLongtitude(120.0);
-        assertEquals(p.getLongtitude(), 120.0);
+        assertEquals(120.0, p.getLongtitude());
     }
 }
